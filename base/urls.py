53e7c0b84bcb34eb5923fbad02e90c8c99549844
alextassy24 <t_alex_00@yahoo.com>
2023-05-07 14:14:33 +0300

    added login and register functionality

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_page, name='logout'),
+    path('register/', views.register_page, name='register'),
+    path('view-data/', views.view_data, name='view-data'),
+    
     
 ]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,59 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.models import User
+from .forms import RegisterForm
+
 
 def home(request):
     return render(request, 'home.html')
+
+def login_page(request):
+    
+    if request.user.is_authenticated:
+        return redirect('home')
+    
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+        
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request,f'User {username} does not exist!')
+            
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            print(user)
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request,f'Username or password are incorrect!')
+
+    return render(request,'login.html')
+
+def logout_page(request):
+    logout(request)
+    return redirect('home')
+
+def register_page(request):
+    form = RegisterForm()
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            messages.success(request, 'Registration successful!')
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration')
+
+    return render(request, 'register.html', {'form': form})
+
+def view_data(request):
+    return render(request, 'view_data.html')
+    
